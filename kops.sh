859c0b23840113e5b920120eddc8ee6818334c5d
kodavara <137081810+kodavara@users.noreply.github.com>
2024-06-20 17:40:24 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -2,6 +2,10 @@
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
 
+apt install unzip -y 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 #! /bin/bash
 aws configure

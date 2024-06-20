4a43ab5d053f971b170307ac06c2ddd9ca368960
kodavara <137081810+kodavara@users.noreply.github.com>
2024-06-20 17:50:27 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -20,3 +20,11 @@ aws s3api put-bucket-versioning --bucket rahamssshaik09.k8s.local --region us-ea
 export KOPS_STATE_STORE=s3://rahamssshaik09.k8s.local
 kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name rahams.k8s.local --yes --admin
+
+
+# validate cluster: kops validate cluster --wait 10m
+# list nodes: kubectl get nodes --show-labels
+# ssh to the master: ssh -i ~/.ssh/id_rsa ubuntu@api.koda.k8s.local
+# the ubuntu user is specific to Ubuntu. If not using Ubuntu please use the appropriate user based on your OS.
+# read about installing addons at: https://kops.sigs.k8s.io/addons.
+
